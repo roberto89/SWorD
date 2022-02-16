cb1884d3b39dfc8c335adc1fde31129854dfb09f
roberto <robertopag@hotmail.it>
2022-02-16 22:23:34 +0100

    first commit test

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   # named routes for static pages and sign up
   get '/about', to: 'pages#About'
   get '/contact', to: 'pages#Contact'
-  get '/faq', to: 'pages#Faq'
-  get '/signup', to: 'pages#SignUp'
-  get '/signin', to: 'pages#SingIn'
+  # get '/faq', to: 'pages#Faq'
+  # get '/signup', to: 'pages#SignUp'
+  # get '/signin', to: 'pages#SingIn'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
